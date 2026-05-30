06ae02a3ba0601b23bf6106bb5b35c657d591324
Kirandeep Gantasala <kirandeepgantasala456@gmail.com>
2026-05-30 11:46:49 +0530

    Added CORS Configuartion for Netlify connection

--- Ecommerce_Backend/src/main/java/com/kirandeep/ecommerce/authentication/security/SecurityConfig.java
+++ Ecommerce_Backend/src/main/java/com/kirandeep/ecommerce/authentication/security/SecurityConfig.java
@@ -53,7 +53,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource(){
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:4200"));
+        config.setAllowedOrigins(List.of("http://localhost:4200","https://kirandeep-ecommerce-application.netlify.app"));
         config.setAllowedMethods(List.of("GET","POST","PUT","PATCH","DELETE"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
